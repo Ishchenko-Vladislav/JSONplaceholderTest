43596155202f85ec6f6c81439a3e995c9966a58f
Ishchenko-Vladislav <patriq.968@gmail.com>
2023-07-26 21:19:57 +0300

    clear code

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,5 @@ export default async function HomePage() {
 const getUsers = async () => {
   const url = "https://jsonplaceholder.typicode.com/users";
   const res = await fetch(url).then((res) => res.json());
-  // setUsers(res);
   return res;
 };
--- components/home/dropdown/DropDown.tsx
+++ components/home/dropdown/DropDown.tsx
@@ -14,9 +14,7 @@ export const DropDown: FC<Props> = ({ users, selected, setSelected }) => {
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
             <span className="block truncate">{selected?.name || "default"}</span>
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              {/* <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /> */}
-            </span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"></span>
           </Listbox.Button>
           <Transition
             as={Fragment}
@@ -40,9 +38,7 @@ export const DropDown: FC<Props> = ({ users, selected, setSelected }) => {
                       {"default"}
                     </span>
                     {selected ? (
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                        {/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
-                      </span>
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"></span>
                     ) : null}
                   </>
                 )}
