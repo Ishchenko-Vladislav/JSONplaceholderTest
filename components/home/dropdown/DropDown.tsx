04f081fb171fdfeb21947444e7da8d544c303003
Ishchenko-Vladislav <patriq.968@gmail.com>
2023-07-26 21:26:42 +0300

    fix dropdown list

--- components/home/dropdown/DropDown.tsx
+++ components/home/dropdown/DropDown.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const DropDown: FC<Props> = ({ users, selected, setSelected }) => {
   return (
-    <div className="fixed top-16 w-72">
+    <div className="max-w-xs w-full py-5">
       <Listbox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
